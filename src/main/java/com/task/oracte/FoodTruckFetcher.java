5b581e9468cb802caf256421d1e986d2e48e9ef2
ernesto a <weskerhluffy@gmail.com>
2020-07-11 11:51:00 -0500

    SE modulariza un mas

--- src/main/java/com/task/oracte/FoodTruckFetcher.java
+++ src/main/java/com/task/oracte/FoodTruckFetcher.java
@@ -39,46 +39,42 @@ public class FoodTruckFetcher {
 
 		log.info("EXECUTING : command line runner");
 
-		var result = client.get()
-				.uri(uriBuilder -> uriBuilder.queryParam("dayorder", today.getDayOfWeek().getValue())
+		var result = fetchInfo(client, today, 0);
+		var offset = 0;
+		while (result.size() > 0) {
+			log.info("Result {}", result);
+			outStream.println(formatResults(result));
+			offset += result.size();
+
+			outStream.println("Press any key to continue . . . ");
+			consoleScanner.nextLine();
+			result = fetchInfo(client, today, offset);
+		}
+	}
+
+	public List<Map<String, String>> fetchInfo(WebClient client, LocalDateTime day, Integer offset) {
+		return client.get()
+				.uri(uriBuilder -> uriBuilder.queryParam("dayorder", day.getDayOfWeek().getValue())
 						.queryParam("$select", "applicant,location").queryParam("$limit", PAGE_SIZE)
-						.queryParam("$where", "start24<'%tR' and end24>'%s'".formatted(today, today))
-						.queryParam("$order", "applicant").build())
+						.queryParam("$where", "start24<'%tR' and end24>'%tR'".formatted(day, day))
+						.queryParam("$order", "applicant").queryParam("$offset", offset).build())
 				.retrieve()
 				// XXX:
 				// https://stackoverflow.com/questions/48598233/deserialize-a-json-array-to-objects-using-jackson-and-webclient
 				.bodyToMono(new ParameterizedTypeReference<List<Map<String, String>>>() {
 				}).block();
-		var offset = 0;
-		while (result.size() > 0) {
-			log.info("Result {}", result);
-			var at = new AsciiTable();
-			at.addRule();
-			at.addRow(PRETTY_COLUMN_NAMES.values());
-			at.addRule();
-			for (Map<String, String> foodTruck : result) {
-//var line = PRETTY_COLUMN_NAMES.keySet().stream().map(c -> foodTruck.get(c)).reduce("", (a, c) -> "%s\t\t%s".formatted(a, c));
-				// var line = PRETTY_COLUMN_NAMES.keySet().stream().map(c -> foodTruck.get(c))
-				// .collect(Collectors.joining("\t\t\t\t\t\t"));
-				at.addRow(PRETTY_COLUMN_NAMES.keySet().stream().map(foodTruck::get).collect(Collectors.toList()));
-				at.addRule();
-			}
-			outStream.println(at.render());
-			offset += result.size();
-
-			outStream.print("Press any key to continue . . . ");
-			consoleScanner.nextLine();
-			final int offset_ = offset;
-			result = client.get()
-					.uri(uriBuilder -> uriBuilder.queryParam("dayorder", today.getDayOfWeek().getValue())
-							.queryParam("$select", "applicant,location").queryParam("$limit", PAGE_SIZE)
-							.queryParam("$where", "start24<'%tR' and end24>'%tR'".formatted(today, today))
-							.queryParam("$order", "applicant").queryParam("$offset", offset_).build())
-					.retrieve()
-					// XXX:
-					// https://stackoverflow.com/questions/48598233/deserialize-a-json-array-to-objects-using-jackson-and-webclient
-					.bodyToMono(new ParameterizedTypeReference<List<Map<String, String>>>() {
-					}).block();
-		}
 	}
+
+	public String formatResults(List<Map<String, String>> foodTrucks) {
+		var at = new AsciiTable();
+		at.addRule();
+		at.addRow(PRETTY_COLUMN_NAMES.values());
+		at.addRule();
+		for (Map<String, String> foodTruck : foodTrucks) {
+			at.addRow(PRETTY_COLUMN_NAMES.keySet().stream().map(foodTruck::get).collect(Collectors.toList()));
+			at.addRule();
+		}
+		return at.render();
+	}
+
 }
