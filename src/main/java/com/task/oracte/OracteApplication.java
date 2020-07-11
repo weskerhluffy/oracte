46f71a0a3330d5f7b3580b8eeb5f80b075a11c46
ernesto a <weskerhluffy@gmail.com>
2020-07-11 11:16:14 -0500

    Separated core logic into its own class

--- src/main/java/com/task/oracte/OracteApplication.java
+++ src/main/java/com/task/oracte/OracteApplication.java
@@ -10,9 +10,11 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -28,14 +30,9 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class OracteApplication implements CommandLineRunner {
 
-	private static final String FOOD_TRUCK_API_URL = "https://data.sfgov.org/resource/jjew-r69b.json";
-	private static final Integer PAGE_SIZE = 10;
-	private static final Map<String, String> PRETTY_COLUMN_NAMES = new LinkedHashMap<>() {
-		{
-			put("applicant", "NAME");
-			put("location", "ADDRESS");
-		}
-	};
+	static final String FOOD_TRUCK_API_URL = "https://data.sfgov.org/resource/jjew-r69b.json";
+	@Autowired
+	private FoodTruckFetcher fetcher;
 
 	public static void main(String[] args) {
 		log.info("STARTING THE APPLICATION");
@@ -45,11 +42,12 @@ public class OracteApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		var today = LocalDateTime.now();
+		fetcher.run(webClient(), consoleScanner(), System.out);
+	}
 
-		log.info("EXECUTING : command line runner");
-
-		var client = WebClient.builder()
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder()
 				// XXX: https://github.com/spring-projects/spring-framework/issues/23961
 				// XXX:
 				// https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/web-reactive.html#webflux-codecs-limits
@@ -57,48 +55,11 @@ public class OracteApplication implements CommandLineRunner {
 						.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(100 * 1024 * 1024)).build())
 				.baseUrl(FOOD_TRUCK_API_URL).defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
 				.build();
-		var result = client.get()
-				.uri(uriBuilder -> uriBuilder.queryParam("dayorder", today.getDayOfWeek().getValue())
-						.queryParam("$select", "applicant,location").queryParam("$limit", PAGE_SIZE)
-						.queryParam("$where", "start24<'%tR' and end24>'%s'".formatted(today, today))
-						.queryParam("$order", "applicant").build())
-				.retrieve()
-				// XXX:
-				// https://stackoverflow.com/questions/48598233/deserialize-a-json-array-to-objects-using-jackson-and-webclient
-				.bodyToMono(new ParameterizedTypeReference<List<Map<String, String>>>() {
-				}).block();
-		var offset = 0;
-		Scanner scan = new Scanner(System.in);
-		while (result.size() > 0) {
-			log.info("Result {}", result);
-			var at = new AsciiTable();
-			at.addRule();
-			at.addRow(PRETTY_COLUMN_NAMES.values());
-			at.addRule();
-			for (Map<String, String> foodTruck : result) {
-//var line = PRETTY_COLUMN_NAMES.keySet().stream().map(c -> foodTruck.get(c)).reduce("", (a, c) -> "%s\t\t%s".formatted(a, c));
-				// var line = PRETTY_COLUMN_NAMES.keySet().stream().map(c -> foodTruck.get(c))
-				// .collect(Collectors.joining("\t\t\t\t\t\t"));
-				at.addRow(PRETTY_COLUMN_NAMES.keySet().stream().map(foodTruck::get).collect(Collectors.toList()));
-				at.addRule();
-			}
-			System.out.println(at.render());
-			offset += result.size();
+	}
 
-			System.out.print("Press any key to continue . . . ");
-			scan.nextLine();
-			final int offset_ = offset;
-			result = client.get()
-					.uri(uriBuilder -> uriBuilder.queryParam("dayorder", today.getDayOfWeek().getValue())
-							.queryParam("$select", "applicant,location").queryParam("$limit", PAGE_SIZE)
-							.queryParam("$where", "start24<'%tR' and end24>'%tR'".formatted(today, today))
-							.queryParam("$order", "applicant").queryParam("$offset", offset_).build())
-					.retrieve()
-					// XXX:
-					// https://stackoverflow.com/questions/48598233/deserialize-a-json-array-to-objects-using-jackson-and-webclient
-					.bodyToMono(new ParameterizedTypeReference<List<Map<String, String>>>() {
-					}).block();
-		}
+	@Bean
+	public Scanner consoleScanner() {
+		return new Scanner(System.in);
 	}
 
 }
